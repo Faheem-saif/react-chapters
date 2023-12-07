a8867c893e8d8c85e5f80a384f153e1f106ea605
Faheem-saif <faheem000saif@gmail.com>
2023-12-08 00:08:07 +0500

    food-app ui

---
+++ Projects/healthy-foodapp/src/components/foodhead.jsx
@@ -0,0 +1,11 @@
+import styles from "./foodhead.module.css"
+
+
+
+export default function Foodhead() {
+  return (
+    <div className={styles.foodhead}>
+     <h1>Healthy Food</h1> 
+    </div>
+  )
+}
